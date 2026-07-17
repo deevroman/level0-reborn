adffc01cbd9cb3261475628b13f620e2e3e2500e
Roman Deev <roman.deev06@gmail.com>
2026-07-17 17:31:24 +0300

    fixes

--- src/js/app.js
+++ src/js/app.js
@@ -212,6 +212,26 @@ function setStatusWithLinks(statusElement, message, links, type = "success") {
     nodes.push(document.createTextNode("."));
   }
 
+  if (links.length > 1) {
+    const externalLinkSvg =
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.25" ' +
+      'stroke-linecap="round" stroke-linejoin="round" ' +
+      'class="lucide lucide-external-link-icon lucide-external-link" width="16" height="16">' +
+      '<path d="M15 3h6v6"></path><path d="M10 14 21 3"></path>' +
+      '<path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>' +
+      "</svg>";
+    const allA = document.createElement("a")
+    allA.innerHTML = externalLinkSvg
+    allA.onclick = e => {
+      e.preventDefault()
+      links.forEach(link => {
+        window.open(link, "_blank")
+      })
+    }
+    nodes.push(document.createTextNode("\xA0"));
+    nodes.push(allA);
+  }
+
   statusElement.dataset.type = type;
   statusElement.replaceChildren(...nodes);
 }
